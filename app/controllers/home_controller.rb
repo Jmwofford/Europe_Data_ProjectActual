cde6cb397ad6fe3c22f8d4c78c6e692469e6cf02
Jordan Wofford <jmwofford@gmail.com>
2018-08-21 22:20:34 -0400

    generated home controller #europe- dbsetup-

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,4 @@
+class HomeController < ApplicationController
+  def europe
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'home/europe'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'home#europe'
+
+end
