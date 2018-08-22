776e205729a61a51a76daa0a4b86d2918b4fb930
Jordan Wofford <jmwofford@gmail.com>
2018-08-21 22:24:06 -0400

    screen_Render_RootPg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
-  def europe
+ 
+ def europe
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/europe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'home#europe'
+    root 'home#europe'
 
 end
